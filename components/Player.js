d5d7c4367bd183be7ee4a711b988672564c5a24e
Skahrr <1dgonascaaron@gmail.com>
2023-08-31 16:14:34 +0100

    player jump done

--- components/Player.js
+++ components/Player.js
@@ -1,16 +1,27 @@
-function Player (x, y) {
-    this.x = x;
-    this.y = y
-    this.createPlayer = function () {
-        let newPlayer = document.createElement("div")
-        newPlayer.setAttribute("id", "player")
-        newPlayer.style.top = this.x + "%"
-        newPlayer.style.left = this.y + "%"
-        document.querySelector("#board").appendChild(newPlayer)
-
+function Player(x, y) {
+  let self = this;
+  this.x = x;
+  this.y = y;
+  this.sprite;
+  this.isJumping = false;
+  this.createPlayer = function () {
+    let newPlayer = document.createElement("div");
+    newPlayer.setAttribute("id", "player");
+    newPlayer.style.top = this.x + "%";
+    newPlayer.style.left = this.y + "%";
+    this.sprite = newPlayer;
+    document.querySelector("#board").appendChild(newPlayer);
+  };
+  this.jump = function (e) {
+    if (e.key === " " && self.isJumping === false) {
+      self.isJumping = true;
+      self.sprite.classList.add("jump");
+      let timer = setTimeout(() => {
+        self.sprite.classList.remove("jump");
+        self.isJumping = false;
+      }, 1010);
     }
+  };
 }
 
-
-
-export { Player }
+export { Player };
--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import { Player } from "./components/Player.js"
 const player = new Player(60, 10);
 
 player.createPlayer()
-
+window.onkeydown = player.jump
 
 
