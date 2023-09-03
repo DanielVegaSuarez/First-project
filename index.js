1cf0949ca002eb5a258db30c5d4acb1956f4a5d0
Skahrr <98775762+Skahrr@users.noreply.github.com>
2023-09-03 15:06:34 +0100

    Revert "time playing  added"

--- index.js
+++ index.js
@@ -1,44 +1,39 @@
 import { Player } from "./components/Player.js";
 import { Obstacle } from "./components/Obstacle.js";
-
-const board = document.querySelector('#board')
-const gameTimer = document.querySelector('#timer')
+const board = document.querySelector("#board");
 const player = new Player(150, 500, board);
-const obstacles = [];
-let timePlaying = 0
+let obstacles = [];
+player.createPlayer();
 
-function startGame(){
-  let timePlayingUp = setInterval(function(){
-    gameTimer.innerText = timePlaying
-    timePlaying++
-    
-  }, 1000)
- 
-  player.createPlayer();
+function obstacleLoop() {
+  let newObstacle = new Obstacle(1100, 680, player);
+  obstacles.push(newObstacle);
 
-  function obstacleLoop() {
-    let newObstacle = new Obstacle(1100, 680, player);
-    obstacles.push(newObstacle);
-      
-      
-      newObstacle.createObstacle();
-      newObstacle.movement();
-      let collisionTimer = setInterval(newObstacle.checkCollision, 30);
-      let timer = setInterval(() => {
-        if (newObstacle.x + player.width + newObstacle.width <= 0) {
-          newObstacle.removeObstacle();
-          clearInterval(collisionTimer);
-          clearInterval(timer);
-          obstacles.shift();
-        }
-      }, 10);
-    ;
-  }
-  let obstacleGenerator = setInterval(obstacleLoop, 3000);
-  
-  window.onkeydown = player.interaction;
-  window.onkeyup = player.stop
+  newObstacle.createObstacle();
+  newObstacle.movement();
+  let collisionTimer = setInterval(newObstacle.checkCollision, 30);
+
+  let timer = setInterval(() => {
+    if (newObstacle.x + player.width + newObstacle.width <= 0) {
+      newObstacle.removeObstacle();
+      clearInterval(collisionTimer);
+      clearInterval(timer);
+      obstacles.shift();
+    }
+  }, 10);
 }
-startGame()
-
-
+let obstacleGenerator = setInterval(obstacleLoop, 5000);
+function gameOver() {
+  if (player.isDead) {
+    clearInterval(obstacleGenerator);
+    alert("game over");
+    obstacles.forEach(obs =>{
+      obs.removeObstacle()
+    })
+    obstacles = []
+    clearInterval(checkDeath);
+  }
+}
+let checkDeath = setInterval(gameOver, 10);
+window.onkeydown = player.interaction;
+window.onkeyup = player.stop;
