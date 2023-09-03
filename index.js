2e0ae7fac2112ddd436e8b66203454a2e2fe0050
Skahrr <1dgonascaaron@gmail.com>
2023-09-03 11:53:12 +0100

    time playing  added

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 import { Player } from "./components/Player.js";
 import { Obstacle } from "./components/Obstacle.js";
 const board = document.querySelector('#board')
+const gameTimer = document.querySelector('#timer')
 const player = new Player(150, 500, board);
 const obstacles = [];
-player.createPlayer();
+let timePlaying = 0
 
-function obstacleLoop() {
-  let newObstacle = new Obstacle(1100, 680, player);
-  obstacles.push(newObstacle);
+function startGame(){
+  let timePlayingUp = setInterval(function(){
+    gameTimer.innerText = timePlaying
+    timePlaying++
     
-    
-    newObstacle.createObstacle();
-    newObstacle.movement();
-    let collisionTimer = setInterval(newObstacle.checkCollision, 30);
-    let timer = setInterval(() => {
-      if (newObstacle.x + player.width + newObstacle.width <= 0) {
-        newObstacle.removeObstacle();
-        clearInterval(collisionTimer);
-        clearInterval(timer);
-        obstacles.shift();
-      }
-    }, 10);
-  ;
+  }, 1000)
+ 
+  player.createPlayer();
+
+  function obstacleLoop() {
+    let newObstacle = new Obstacle(1100, 680, player);
+    obstacles.push(newObstacle);
+      
+      
+      newObstacle.createObstacle();
+      newObstacle.movement();
+      let collisionTimer = setInterval(newObstacle.checkCollision, 30);
+      let timer = setInterval(() => {
+        if (newObstacle.x + player.width + newObstacle.width <= 0) {
+          newObstacle.removeObstacle();
+          clearInterval(collisionTimer);
+          clearInterval(timer);
+          obstacles.shift();
+        }
+      }, 10);
+    ;
+  }
+  let obstacleGenerator = setInterval(obstacleLoop, 3000);
+  
+  window.onkeydown = player.interaction;
+  window.onkeyup = player.stop
 }
-let obstacleGenerator = setInterval(obstacleLoop, 3000);
+startGame()
 
-window.onkeydown = player.interaction;
-window.onkeyup = player.stop
